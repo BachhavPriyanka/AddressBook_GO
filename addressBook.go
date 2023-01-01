e29a7ae015f3a57f1228161543ed0a4667e41b96
admin <bachhavpriyanka09@gmail.com>
2023-01-01 15:20:12 +0530

    Added: Address book contact with fname,lname..etc

--- addressBook.go
+++ addressBook.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type Contact struct {
+	FirstName   string
+	LastName    string
+	Address     string
+	City        string
+	State       string
+	PhoneNumber string
+	Email       string
+}
+
+func (a Contact) Display() {
+	fmt.Printf("\nFirst Name:%s\nLast Name:%s\nAddress:%s\nCity:%s\nState:%s\nPhone Number:%s\nEmail:%s", a.FirstName, a.LastName, a.Address, a.City, a.State, a.PhoneNumber, a.Email)
+}
+
 func main() {
-	fmt.Println("*******Welcome to Address Book Program in GO Lang*******")
+	fmt.Print("********Address book main**********")
+
+	contact1 := Contact{
+		FirstName:   "Priyanka",
+		LastName:    "Bachhav",
+		Address:     "Township",
+		City:        "Pune",
+		State:       "MH",
+		PhoneNumber: "9878564576",
+		Email:       "bachhav@gmail.com"}
+
+	contact1.Display()
+
 }
